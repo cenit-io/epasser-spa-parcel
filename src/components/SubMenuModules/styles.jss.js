3546c68a5d739f41978f90df95ecd4ed577d88ca
yoandrypa <yoandrypa@gmail.com>
2021-09-25 01:49:07 -0400

    Update styles

--- src/components/SubMenuModules/styles.jss.js
+++ src/components/SubMenuModules/styles.jss.js
@@ -13,6 +13,21 @@ const styles = (theme) => ({
       color: theme.palette.text.primary,
       backgroundColor: theme.palette.primary.light,
       textTransform: 'uppercase',
+    },
+
+    '& .MuiAccordionDetails-root': {
+      display: 'block',
+      padding: 8,
+    },
+
+    '& .MuiListItem-gutters': {
+      paddingLeft: 8,
+      paddingRight: 8,
+    },
+
+    '& .MuiListItem-root': {
+      paddingTop: 4,
+      paddingBottom: 4,
     }
   },
 
