3058cf40444c39b71dfc19743f27d5e332e02637
yoandrypa <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>
2022-05-16 18:02:15 -0400

    Update styles

--- src/components/AbstractPageDetails/styles.jss.js
+++ src/components/AbstractPageDetails/styles.jss.js
@@ -22,10 +22,9 @@ const styles = (theme) => ({
     borderColor: theme.palette.primary.dark,
     borderRadius: theme.shape.borderRadius,
     // borderStyle: 'ridge',
-    borderWidth: 0,
+    borderWidth: 1,
     marginBottom: theme.spacing(2),
     width: 'calc(100% - 16px)',
-    boxShadow: '0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)',
 
     '& > legend': {
       textAlign: 'left',
